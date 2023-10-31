76cdd00fd2917d0a93e81b762ff372501a7059a4
Ruekah <emmanuelruto562@gmail.com>
2023-10-31 08:06:40 +0300

    First commit

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    path('sign-in/', auth_views.LoginView.as_view(template_name='base/sign_in.html')),
+    path('sign-out/', auth_views.LogoutView.as_view(next_page='/')),
+    path('sign-up/', views.signUp),
     path('customer/', views.customerPage, name='customer'),
     path('courier/', views.courierPage, name='courier'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from pdb import post_mortem
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
 
@@ -7,9 +12,30 @@ def home(request):
     return render(request, 'base/home.html')
 
 
+@login_required()
 def customerPage(request):
     return render(request, 'base/home.html')
 
 
+@login_required()
 def courierPage(request):
     return render(request, 'base/home.html')
+
+
+def signUp(request):
+    form = forms.SignUpForm
+    if request.method == 'POST':
+        form = forms.SignUpForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data.get('email').lower()
+
+            user = form.save(commit=False)
+            user.username = email
+            user.save()
+
+            login(request, user)
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'base/sign_up.html', context)
